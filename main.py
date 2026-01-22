9188285de05c82739f2ad49f7ba4354bc56318fe
NISHAN <nishan@tbdir.net>
2026-01-22 10:56:37 +0530

    Committing Capture from Camera through Raspberry Pi set up

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from anomaly_graph import router as anomaly_router
 from yolo_image import router as yolo_image
+#from camera import router as camera_router
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -10,5 +11,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(anomaly_router)
+
 app.include_router(yolo_image)
+#app.include_router(camera_router)
